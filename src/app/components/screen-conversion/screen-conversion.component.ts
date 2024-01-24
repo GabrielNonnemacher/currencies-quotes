6287096cddad7c1d7c18cb4083f0d3ca4665c286
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2024-01-23 22:26:56 -0300

    Task #12 - estilização do input de numero

--- src/app/components/screen-conversion/screen-conversion.component.ts
+++ src/app/components/screen-conversion/screen-conversion.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { URL_HELPER } from 'src/app/helpers/constants/url.constant';
 
 @Component({
   selector: 'quote-screen-conversion',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./screen-conversion.component.scss']
 })
 export class ScreenConversionComponent {
-
+  iconConvertion = signal<string>(URL_HELPER.icons.convertion);
 }
--- src/app/helpers/constants/url.constant.ts
+++ src/app/helpers/constants/url.constant.ts
@@ -2,6 +2,7 @@ export const URL_HELPER = {
     icons: {
         themeDark: '../../../../assets/images/moon.svg',
         themeLight: '../../../../assets/images/sun.svg',
-        logoHeader: '../../../../assets/images/logo.svg'
+        logoHeader: '../../../../assets/images/logo.svg',
+        convertion: '../../../../assets/images/convertion.svg'
     }
 }
